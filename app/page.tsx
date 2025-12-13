5b34576f43d6bb8b0b96e1ee741cc7eb44e50d91
Edi43535 <edvinjashari23@gmail.com>
2025-12-13 21:01:57 +0100

    übung 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 // app/page.tsx
-import React from "react";
+"use client";
+
+import { useState } from "react";
 
 export default function Page() {
+  const [count, setCount] = useState(0);
+
+  // normale Funktion
+  function increment() {
+    setCount(count + 1);
+  }
+
+  // Arrow Function
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <main
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        margin: 0,
-        fontFamily:
-          "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial",
-      }}
-    >
-      <h1>Hello World</h1>
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{count}</h1>
+
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </main>
   );
 }
